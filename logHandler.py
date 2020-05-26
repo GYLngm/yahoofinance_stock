498fd4d7005a7f0709aa6668c0af0912e0401a97
GYLngm <morgan21cn@gmail.com>
2020-05-26 20:51:54 +0200

    2020/05/26 new version with pandas

--- logHandler.py
+++ logHandler.py
@@ -1,5 +1,6 @@
 import time
 import os
+import pathlib
 
 
 class LogHandler:
@@ -8,29 +9,51 @@ class LogHandler:
     def log_msg(msg):
         now = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(int(round(time.time() * 1000)) / 1000))
         filenow = time.strftime('%Y%m%d', time.localtime(int(round(time.time() * 1000)) / 1000))
-        log_msg = "[%s] %s" % (
+        log_msg = "[%s] %s\r\n" % (
             now,
             msg
         )
-        directory = "log"
+        directory = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".") + "\\log"
+
+        # fp = pathlib.Path().absolute()+"\\"+directory+"\\"
         if not os.path.exists(directory):
             os.makedirs(directory)
-        with open("./%s/%s" % (
-                directory,
-                "log_" + filenow + ".log"
-        ), "a", encoding='utf-8') as f:
+
+        with open(
+                directory + "\\log_" + filenow + ".log",
+                "a", encoding='utf-8') as f:
             f.write(log_msg)
         print(msg)
 
     @staticmethod
     def log_properties(filename, p):
         now = time.strftime('%Y/%m/%d', time.localtime(int(round(time.time() * 1000)) / 1000))
-        directory = "./Fields_not_in_definition/"
+        directory = os.path.abspath(
+            os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".") + "\\Fields_not_in_definition"
         if not os.path.exists(directory):
             os.makedirs(directory)
-        directory += filename + ".log"
 
-        with open(directory, "w+", encoding='utf-8') as f:
+        with open(
+                directory + "\\" + filename + ".log", "a+", encoding='utf-8') as f:
             line_found = any(p in line for line in f)
             if not line_found:
-                f.write(p + "\n")
+                f.write(p + "\r\n")
+
+    @staticmethod
+    def log_exceptions(msg):
+        now = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(int(round(time.time() * 1000)) / 1000))
+        filenow = time.strftime('%Y%m%d', time.localtime(int(round(time.time() * 1000)) / 1000))
+        log_msg = "[%s] %s\r\n" % (
+            now,
+            msg
+        )
+        directory = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".") + "\\log"
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        with open(
+                directory + "\\exceptions_" + filenow + ".log",
+                "a", encoding='utf-8') as f:
+            f.write(log_msg)
+        print(msg)
--- mytools.py
+++ mytools.py
@@ -1,18 +1,29 @@
 import re
-import time
-import os
-from datetime import datetime
-from logHandler import LogHandler
-import numpy as np
+import sqlalchemy
+from sqlalchemy.exc import DBAPIError
 
-from dbConnection import dbConnection
+from orm.dbConnection import dbConnection
+from logHandler import LogHandler
+from dbConnectionEngine import dbConnectionEngine
 
 
 class myTools:
     __con = None
+    __sqlCon = None
+    mp = {
+        'yahoofinance_stock_balance_sheet': (),
+        'yahoofinance_stock_income_statement': (),
+        'yahoofinance_stock_price': (),
+        'yahoofinance_stock_valuation_measures': (),
+    }
 
     def __init__(self):
-        self.__con = dbConnection()
+        LogHandler.log_msg("Initializing tools..")
+        self.__con = dbConnectionEngine()
+        self.__sqlCon = dbConnection()
+        LogHandler.log_msg("Fetch current table attribute")
+        self.__con.loadModelProperties(self.mp)
+        LogHandler.log_msg("Done.")
     
     def matchFile(self, filename, **ar):
         property = {'cols': {}, 'rows': [], 'key': '', 'filename': filename}
@@ -55,6 +66,9 @@ class myTools:
             property['table'] = 'yahoofinance_stock_balance_sheet'
             return property
 
+    def getMp(self):
+        return self.mp
+
     def checkIfIsDate(self, Columns):
         searchObj = re.search(r'(\d{2}/\d{2}/\d{4})|(ttm)', Columns[1], re.M | re.I)
         if searchObj:
@@ -68,49 +82,51 @@ class myTools:
         return res
 
     def regroupRowsFromDict(self, dr, c, fileProperty):
-        args = []
-        for i in range(1, len(c)):
-            tmp = {}
-            for x in range(len(dr[c[i]])):
-                mp = self.__con.getModelProperties()
-                if dr['name'][x].strip() in mp[fileProperty['table']]:
-                    tmp[dr['name'][x].strip()] = self.convertNumberWithCommaInArray(dr[c[i]][x])
-                    tmp['Code'] = fileProperty['cols']['Code']
-                    if c[i] == 'ttm':
-                        continue
-                    else:
-                        tmp['ReportDate'] = datetime.strptime(c[i], '%m/%d/%Y').strftime('%Y-%m-%d')
-                    if fileProperty['key'] != 'balance':
-                        tmp['ValuationMethod'] = fileProperty['cols']['ValuationMethod']
-                else:
-                    # print(dr['name'][x].strip())
-                    LogHandler.log_properties(filename=fileProperty['filename'], p=dr['name'][x].strip())
-            args = np.append(args, tmp)
-        return args
+        pass
 
     def regroupRowsFromDictForPrice(self, dr, fileProperty):
+        pass
+
+    def save(self, table, dataframe):
+        self.__sqlCon.insert_update_from_dataframe(dataframe=dataframe, table=table)
+        pass
+
+    def saveDataFrame(self, dataframe, table):
+        try:
+            dataframe.to_sql(name=table, con=self.__con.getEngine(),
+                             index=False,
+                             if_exists='append',
+                             dtype=self.__con.getDbType()[table])
+        except DBAPIError as e:
+            LogHandler.log_exceptions("Sql Exceptions: %s\r\n" % e)
+            pass
+        finally:
+            pass
+
+    # Development function DEBUG USAGE Not important
+    def generateTabelOnConsole(self, table, df):
+        fields = []
         args = []
-        for i in range(len(dr)):
-            dr[i]['Code'] = fileProperty['cols']['Code']
-            tmp = {}
-            for x in range(len(dr[i])):
-                key = list(dr[i].keys())[x].replace(' ', '')
-                tmp[key] = dr[i][list(dr[i].keys())[x]]
-            args = np.append(args, tmp)
-        return args
-
-    def saveData(self, args, table):
-        flag = False
-        """"
-        for c in range(len(list(args[0].keys()))):
-            self.__con.alter(table=table, col=list(args[0].keys())[c])
-        """
-        for i in range(len(args)):
-            flag = self.__con.insert(table=table,
-                                     cols=list(args[i].keys()),
-                                     rows=list(args[i].values()),
-                                     org_dict=args[i])
-        self.__con.getConnect().commit()
-
-    def closeDbConnection(self):
-        self.__con.closeConnection()
+        for x in self.__con.getDbType()[table]:
+            type = self.__con.getDbType()[table][x]
+            type_str = ''
+            if type == sqlalchemy.types.Date:
+                type_str = 'DATE PRIMARY KEY NOT NULL'
+                args.append('`%s`' % x)
+            elif type == sqlalchemy.types.String:
+                type_str = 'VARCHAR(50) PRIMARY KEY NOT NULL'
+                args.append('`%s`' % x)
+            elif type == sqlalchemy.types.Float:
+                type_str = 'FLOAT'
+            fields.append(("`%s` %s" % (x, type_str)))
+        print("""
+            CREATE TABLE `%s`(
+                %s,
+                PRIMARY KEY (%s)
+            )
+        """ % (
+            table,
+            ",".join(fields),
+            ",".join(args)
+        ))
+        pass
